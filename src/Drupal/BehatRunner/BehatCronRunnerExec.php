c3b3bb68b74bc7965a7610dabacb9d4222a8ae29
alnutile <alfrednutile@gmail.com>
2013-10-07 13:37:57 -0400

    Fixes #3 should now be able to detect and run on Saucelabs

--- src/Drupal/BehatRunner/BehatCronRunnerExec.php
+++ src/Drupal/BehatRunner/BehatCronRunnerExec.php
@@ -16,6 +16,9 @@ class BehatCronRunnerExec extends BehatEditorRun {
             $this->yml_path = drupal_realpath($path) . '/behat/behat.yml';
             $this->saucelabs = TRUE;
         }
+        watchdog('test_sl_exists', print_r(module_exists('behat_editor_saucelabs'), 1));
+        watchdog('test_sl_path', print_r($this->yml_path, 1));
+
     }
 
     public function exec($javascript = FALSE) {
@@ -24,9 +27,15 @@ class BehatCronRunnerExec extends BehatEditorRun {
         } else {
             $tags = "--tags '~@javascript'";
         }
+
+        if($this->saucelabs == TRUE) {
+            $profile = '--profile=Webdriver-saucelabs';
+        } else {
+            $profile = '';
+        }
         //Needed just 1 or 0 for results
         // could not get the $results value of exec to do that
-        exec("cd $this->behat_path && ./bin/behat --config=\"$this->yml_path\" --no-paths $tags $this->absolute_file_path", $output, $results);
+        exec("cd $this->behat_path && ./bin/behat --config=\"$this->yml_path\" $profile --no-paths $tags $this->absolute_file_path", $output, $results);
         variable_set('behat_cron_runner_last_run', REQUEST_TIME);
         return array('results' => $results, 'output' => $output);
     }
