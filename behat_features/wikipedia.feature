6e93debaff5a9140d7069fc58b74c53b257ad598
alnutile <alfrednutile@gmail.com>
2013-11-04 14:09:10 -0500

    Bug in critical tag search

---
+++ behat_features/wikipedia.feature
@@ -0,0 +1,11 @@
+ @critical
+ Feature: Example Test for WikiPedia
+
+   @anonymous @external
+   Scenario: WikiPedia
+     Given I am on "http://en.wikipedia.org/wiki/Main_Page"
+     Then I should see "WikiPedia"
+     And I follow "Donate"
+     Given I ponder life
+     Then I should see "Thanks"
+     Then I should not see "Muffins"
--- src/Drupal/BehatRunner/Tests.php
+++ src/Drupal/BehatRunner/Tests.php
@@ -3,13 +3,17 @@
 namespace Drupal\BehatRunner;
 
 use Drupal\BehatEditor\Files,
-    Drupal\BehatRunner;
+    Drupal\BehatRunner,
+    Drupal\BehatEditor;
 
 class Tests {
     public $allCriticalTests = array();
 
     public function __construct() {
+        composer_manager_register_autoloader();
         $this->allCriticalTests = self::getCriticalTests();
+
+        watchdog('test_line_16', print_r($this->allCriticalTests, 1));
     }
 
     /**
@@ -20,20 +24,31 @@ class Tests {
      *
      */
     public function getCriticalTests() {
-        if($cache = cache_get('behat_runner_tests')) {
+
+        $cache = cache_get('behat_runner_tests');
+        if(cache_get('behat_runner_tests')) {
             return $cache->data;
         } else {
+            watchdog('test_line_32', print_r(self::_setCriticalTestsCache(), 1));
             return self::_setCriticalTestsCache();
         }
     }
 
+    /**
+     * Run the tests but if fail (status = 1)
+     * run a second time.
+     *
+     * @param int $javascript
+     * @return array
+     */
     public function runTests($javascript = 0) {
         $results = array();
         foreach($this->allCriticalTests as $key) {
             $run = 1;
             $test = self::_test_each($key, 1, $javascript);
             $status = $test['results'];
-            if($status === 1 && $run == 1) {
+            $fail = 1;
+            if($status === $fail && $run == 1) {
                 $run = 2;
                 $test = self::_test_each($key, $run, $javascript);
                 $duration = array_pop($test['output']);
@@ -48,15 +63,14 @@ class Tests {
     }
 
     private function _test_each($key, $run, $javascript) {
-        $res = new BehatCronRunnerExec($key);
+        //$res = new BehatCronRunnerExec($key);
+        $res = new BehatEditor\BehatEditorRun($key);
         $test = $res->exec($javascript);
         return $test;
     }
 
 
     private function _setCriticalTestsCache(){
-        composer_manager_register_autoloader();
-
         $get = new Files();
         $data = $get->getFilesArray();
         $dataTmp = self::_filterCritical($data);
@@ -66,9 +80,16 @@ class Tests {
     private function _filterCritical($files_array) {
         $criticals = array();
         foreach($files_array as $key => $value) {
+            watchdog('test_line_83', print_r($key, 1));
             foreach($value as $file_path => $values) {
-                if(in_array('@critical', $values['tags_array'])) {
-                    $criticals[$file_path] = $values;
+                watchdog('test_line_84', print_r($values['tags_array'], 1));
+                foreach($values['tags_array'] as $tag) {
+                    //in_array not working for some reason?
+                    watchdog("tag", print_r(substr($tag, 1), 1));
+                    if(substr($tag, 1) == 'critical') {
+                        watchdog("found_in_array", print_r('test'));
+                        $criticals[$file_path] = $values;
+                    }
                 }
             }
         }
