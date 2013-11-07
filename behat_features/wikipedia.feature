05fee95cc7bbfdcae0c4861995fa3ab7231f8c06
basurveycom@gmail.com <basurveycom@gmail.com>
2013-11-07 10:54:44 -0500

    Seems again I need to update the pass fail repsonse for this. It was looking for it in results but it was passed in response key

--- behat_features/wikipedia.feature
+++ behat_features/wikipedia.feature
@@ -1,11 +1,10 @@
  @critical
  Feature: Example Test for WikiPedia
 
-   @javascript @anonymous @external
+   @anonymous @external
    Scenario: WikiPedia
      Given I am on "http://en.wikipedia.org/wiki/Main_Page"
      Then I should see "WikiPedia"
      And I follow "Donate"
-     Given I ponder life
-     Then I should see "Thanks"
+     Then I should see "Muffins"
      Then I should not see "Muffins"
--- src/Drupal/BehatRunner/Tests.php
+++ src/Drupal/BehatRunner/Tests.php
@@ -43,13 +43,13 @@ class Tests {
         foreach($this->allCriticalTests as $key) {
             $run = 1;
             $test = self::_test_each($key, 1, $javascript);
-            $status = $test['results'];
+            $status = $test['response'];
             $fail = 1;
             if($status === $fail && $run == 1) {
                 $run = 2;
                 $test = self::_test_each($key, $run, $javascript);
                 $duration = array_pop($test['output_array']);
-                $status = $test['results'];
+                $status = $test['response'];
                 $results[] = array('file_object' => $key, 'status' => $status, 'duration' => $duration, 'time' => time());
             } else {
                 $duration = array_pop($test['output_array']);
